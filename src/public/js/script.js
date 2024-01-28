856777993d86b64b0c0752deedd4290ac272eef5
kisakot8 <kisakotdad@gmail.com>
2024-01-28 20:38:08 +0100

    Correct aspect ratio of article thumbnails

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 
 function init() {
-    for (let i = 1; i <= articleCount; i++) {
+    for (let i = 0; i < articleCount; i++) {
         createArticleDiv(i);
     }
 }
@@ -16,14 +16,19 @@ function createArticleDiv(index) {
 
     const thumbnail = document.createElement("img");
     thumbnail.className = 'article-thumbnail';
-    thumbnail.src = `./articles/${index}/thumbnail.webp`;
+    thumbnail.src = `./articles/${index+1}/thumbnail.webp`;
 
     const title = document.createElement("h2");
     title.className = 'article-title';
-    title.textContent = titles[index-1];
+    title.textContent = titles[index];
+
+    const author = document.createElement("h4");
+    author.className = 'article-author';
+    author.textContent = `By ${authors[index]}`;
 
     articleDiv.appendChild(thumbnail);
     articleDiv.appendChild(title);
+    articleDiv.appendChild(author);
 
     articlesContainer.appendChild(articleDiv);
 }
