537c61e8e39ecff28fcdc5d2e83583f26d13956e
kisakot8 <kisakotdad@gmail.com>
2024-01-29 10:30:45 +0100

    Contained image

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,32 +1,37 @@
-const articlesContainer = document.querySelector(".articles-container");
+const articlesContainer = document.querySelector('.articles-container');
 
 document.addEventListener('DOMContentLoaded', init);
 
 
 function init() {
     for (let i = 0; i < articleCount; i++) {
-        createArticleDiv(i);
+        articleConstructor(i);
     }
 }
 
 
-function createArticleDiv(index) {
-    const articleDiv = document.createElement("div");
-    articleDiv.className = "article-card";
+function articleConstructor(index) {
+    const articleDiv = document.createElement('div');
+    articleDiv.className = 'article-card';
 
-    const thumbnail = document.createElement("img");
+    const thumbnailContainer = document.createElement('div');
+    thumbnailContainer.className = 'article-thumbnail-container';
+
+    const thumbnail = document.createElement('img');
     thumbnail.className = 'article-thumbnail';
     thumbnail.src = `./articles/${index+1}/thumbnail.webp`;
 
-    const title = document.createElement("h2");
+    const title = document.createElement('h2');
     title.className = 'article-title';
     title.textContent = titles[index];
 
-    const author = document.createElement("h4");
+    const author = document.createElement('h4');
     author.className = 'article-author';
     author.textContent = `By ${authors[index]}`;
 
-    articleDiv.appendChild(thumbnail);
+    thumbnailContainer.appendChild(thumbnail);
+
+    articleDiv.appendChild(thumbnailContainer);
     articleDiv.appendChild(title);
     articleDiv.appendChild(author);
 
