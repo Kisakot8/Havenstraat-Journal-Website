8c3cf5cedfbc6748a37a8a31ab7917b8f4eddea8
kisakot8 <kisakotdad@gmail.com>
2024-01-30 11:22:52 +0100

    Can show articles by tag

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -15,6 +15,11 @@ function articleConstructor(index) {
     articleDiv.className = 'article-card';
     articleDiv.id = `article-${index+1}`;
 
+    for (const j in data[index+1]['tags']) {
+        const tag = data[index+1]['tags'][j];
+        articleDiv.classList.add(tag);
+    }
+
 
     const thumbnailContainer = document.createElement('div');
     thumbnailContainer.className = 'article-thumbnail-container';
@@ -39,4 +44,22 @@ function articleConstructor(index) {
     articleDiv.appendChild(author);
 
     articlesContainer.appendChild(articleDiv);
+}
+
+
+function showByTag(tag) {
+    all = document.getElementsByClassName('article-card');
+
+    for (let index = 0; index < all.length; index++) {
+        const article = all[index];
+        if (!article.classList.contains(tag)) {
+            article.style.display = 'none';
+        } else { article.style.display = 'flex'; }
+    }
+
+    prevLink = document.querySelector('.current-section');
+    prevLink.classList.remove('current-section');
+
+    newLink = document.getElementById(`${tag}-link`);
+    newLink.classList.add('current-section');
 }
