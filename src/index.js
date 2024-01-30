0600fad720aa2a1389ae34928657d39aad051ab3
kisakot8 <kisakotdad@gmail.com>
2024-01-30 10:48:22 +0100

    Article data now stored as JSON and not .txt's

--- src/index.js
+++ src/index.js
@@ -1,45 +1,42 @@
 const { readFile, readFileSync, readdir, readdirSync } = require('fs');
 const express = require('express');
+const e = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-
-const articlesDir = './public/articles';
 const splitter = '%%%';
+const articlesDir = './public/articles';
 
 readdir(articlesDir, (err, files) => {
     global.articleCount = files.length;
 });
 
 
+
 app.get('/', (req, res) => {
-    var titles = "";
-    var authors = "";
     var thumbnailExts = "";
+    var allData = {};
     for (let i = 1; i <= articleCount; i++) {
-        const title = readFileSync(`${articlesDir}/${i}/title.txt`, 'utf-8');
-        titles += title + splitter;
 
-        const author = readFileSync(`${articlesDir}/${i}/author.txt`, 'utf-8');
-        authors += author + splitter;
-
+        // Get thumbnail file extension
         const files = readdirSync(`${articlesDir}/${i}`);
         for (const file of files) {
             splitFile = file.split('.');
-            if (splitFile.slice(0, -1).join('.') === 'thumbnail') {
-                thumbnailExts += splitFile[splitFile.length - 1] + splitter;
-            }
+            if (splitFile.slice(0, -1).join('.') === 'thumbnail') { thumbnailExts += splitFile[splitFile.length - 1] + splitter; }
         }
+
+        // Get JSON
+        data = JSON.parse(readFileSync(`${articlesDir}/${i}/details.json`, 'utf-8'));
+        allData[`${i}`] = data;
     }
 
     res.render('index', { // render the main file & transfer variables from server to client
-        'titles' : titles,
-        'authors': authors,
-        'thumbnailExts' : thumbnailExts,
-        'articleCount' : articleCount,
         'splitter' : splitter,
+        'articleCount' : articleCount,
+        'thumbnailExts' : thumbnailExts,
+        'data' : allData,
     }); 
 });
 
--- src/public/js/script.js
+++ src/public/js/script.js
@@ -13,6 +13,8 @@ function init() {
 function articleConstructor(index) {
     const articleDiv = document.createElement('div');
     articleDiv.className = 'article-card';
+    articleDiv.id = `article-${index+1}`;
+
 
     const thumbnailContainer = document.createElement('div');
     thumbnailContainer.className = 'article-thumbnail-container';
@@ -21,15 +23,16 @@ function articleConstructor(index) {
     thumbnail.className = 'article-thumbnail';
     thumbnail.src = `./articles/${index+1}/thumbnail.${thumbnailExts[index]}`;
 
+    thumbnailContainer.appendChild(thumbnail);
+
+
     const title = document.createElement('h2');
     title.className = 'article-title';
-    title.textContent = titles[index];
+    title.textContent = data[index+1]['title'];
 
     const author = document.createElement('h4');
     author.className = 'article-author';
-    author.textContent = `By ${authors[index]}`;
-
-    thumbnailContainer.appendChild(thumbnail);
+    author.textContent = `By ${data[index+1]['author']}`;
 
     articleDiv.appendChild(thumbnailContainer);
     articleDiv.appendChild(title);
