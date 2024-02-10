847297ad3b7a337ad6b5bc1519427c6549f5d823
kisakot8 <kisakotdad@gmail.com>
2024-02-10 14:04:33 +0100

    Create article cards from Sanity

--- src/_includes/components/card.js
+++ src/_includes/components/card.js
@@ -3,39 +3,13 @@ const {html} = require('common-tags');
 function card(title, imgUrl, tags, author) {
     return html`
     <div class="article-card ${tags.join(' ')}">
+        <div class="article-thumbnail-container">
+            <img class="article-thumbnail" src=${imgUrl} alt="Article thumbnail"></img>
+        </div>
+        <h2 class="article-title">${title}</h2>
+        <h4 class="article-author">By ${author.name}</h4>
     </div>
     `
-    const articleDiv = document.createElement('div');
-    articleDiv.className = 'article-card';
-
-    for (let tag of tags) {
-        articleDiv.classList.add(tag);
-    }
-
-
-    const thumbnailContainer = document.createElement('div');
-    thumbnailContainer.className = 'article-thumbnail-container';
-
-    const thumbnail = document.createElement('img');
-    thumbnail.className = 'article-thumbnail';
-    thumbnail.src = imgUrl;
-
-    thumbnailContainer.appendChild(thumbnail);
-
-
-    const titleElement = document.createElement('h2');
-    titleElement.className = 'article-title';
-    titleElement.textContent = title;
-
-    const authorElement = document.createElement('h4');
-    authorElement.className = 'article-author';
-    authorElement.textContent = `By ${author.name}`;
-
-    articleDiv.appendChild(thumbnailContainer);
-    articleDiv.appendChild(titleElement);
-    articleDiv.appendChild(authorElement);
-
-    articlesContainer.appendChild(articleDiv);
 }
 
 module.exports = card;
