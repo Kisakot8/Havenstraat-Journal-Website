ce08a08ed4bc2d65205925757051a8d65a7f45c9
kisakot8 <kisakotdad@gmail.com>
2024-02-13 14:58:14 +0100

    Navbar sections now show articles by tag on click

--- src/_includes/components/tag.js
+++ src/_includes/components/tag.js
@@ -2,7 +2,7 @@ const {html} = require('common-tags');
 
 function tag(name) {
     return html`
-    <a class="navbar-links" id="${name}-link"> ${name} </a>
+    <a class="navbar-links" id="${name}-link" href="javascript:showByTag('${name}')"> ${name} </a>
     `
 }
 
