cf98f6fbbec22758da6307bc15cf6fa4f14820a6
kisakot8 <kisakotdad@gmail.com>
2024-02-21 12:24:08 +0100

    Added cycling default text in results

--- src/js/search.js
+++ src/js/search.js
@@ -1,3 +1,5 @@
+// #region select-all
+
 $('#issue-selector-all').click(function(event) {   
     if(this.checked) {
         $('.issue-checkbox').each(function() {
@@ -35,3 +37,42 @@ $('#tag-selector-all').click(function(event) {
         });
     }
 });
+
+// #endregion
+
+// #region text-cycle
+
+const shuffle = (array) => { 
+    return array.sort(() => Math.random() - 0.5); 
+}; 
+
+const CYCLE_INTERVAL = 3000; // in ms
+
+let cycleItems = shuffle(Array.from(document.getElementsByClassName('cycle-item')));
+cycleIndex = 0;
+
+let cycleItem = cycleItems[cycleIndex];
+
+const cycle = setInterval(() => {
+    cycleItem.classList.remove('cycle-item-appearing', 'cycle-item-disappearing');
+    cycleItem = cycleItems[cycleIndex];
+
+    // Assign random color to the item
+    if (!cycleItem.style.color) {
+    cycleItem.style.color = '#'+Math.floor(Math.random()*16777215).toString(16);
+    }
+
+    cycleItem.classList.add('cycle-item-appearing');
+
+    setTimeout(() => {
+        cycleItem.classList.add('cycle-item-disappearing');
+    }, CYCLE_INTERVAL/2);
+
+    if (cycleIndex === cycleItems.length-1) {
+        cycleIndex = 0;
+    } else {
+        cycleIndex += 1;
+    }
+}, CYCLE_INTERVAL);
+
+// #endregion
