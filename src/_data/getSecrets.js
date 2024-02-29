e7f51876b48e0d3a5060421655303a6f925cba44
kisakot8 <kisakotdad@gmail.com>
2024-02-29 22:25:51 +0100

    Updated footer links and copyright notice

--- src/_data/getSecrets.js
+++ src/_data/getSecrets.js
@@ -4,14 +4,10 @@ require('dotenv').config();
 
 function getSecrets () {
     const NAME = process.env.NAME;
-    const DESIGNER = process.env.DESIGNER;
-    const DESIGNER_LINK = process.env.DESIGNER_LINK;
     const INSTA_LINK = process.env.INSTA_LINK;
 
     return {
         "name": NAME,
-        "designer": DESIGNER,
-        "designer_link": DESIGNER_LINK,
         "insta_link": INSTA_LINK
     }
 }
