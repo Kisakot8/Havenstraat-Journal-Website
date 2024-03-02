b955a031d125ab92f348fcce64b36da0386a1539
kisakot8 <kisakotdad@gmail.com>
2024-03-02 15:19:22 +0100

    Added about page leaders section

--- src/_includes/components/aboutHead.js
+++ src/_includes/components/aboutHead.js
@@ -5,16 +5,22 @@ function aboutHead(role, members, imgUrl, order) {
 
     for (let index = 0; index < members.length; index++) {
         const member = members[index];
-        leaderBios += `\n<li>${member.name} - ${member.bio}</li>`
+        leaderBios += `\n<li><span class="leader-name">${member.name}</span> - ${member.bio}</li>`
     }
 
     return html`
     <div class="about-leaders" style="order: ${order};">
-        <h2>${role}</h2>
-        <img src="${imgUrl}" alt="A photo of confident-looking people">
-        <ul class="leaders-bios">
-            ${leaderBios}
-        </ul>
+        <div class="leaders-info">
+            <div class="leaders-visual">
+                <div class="leaders-img-container">
+                    <img src="${imgUrl}" alt="A photo of confident-looking people">
+                </div>
+                <h2>${role}</h2>
+            </div>
+            <ol class="leaders-bios">
+                ${leaderBios}
+            </ol>
+        </div>
     </div>
     `
 }
