5b6135c1a34b99e4ff68b1ecc12f9d842228485a
kisakot8 <kisakotdad@gmail.com>
2024-03-05 14:29:40 +0100

    Ordered about lists and made width consistent

--- src/_includes/components/aboutHead.js
+++ src/_includes/components/aboutHead.js
@@ -3,6 +3,8 @@ const {html} = require('common-tags');
 function aboutHead(role, members, imgUrl, order) {
     let leaderBios = '';
 
+    members.sort((a, b) => a.name.localeCompare(b.name));
+
     for (let index = 0; index < members.length; index++) {
         const member = members[index];
         leaderBios += `\n<li><span class="leader-name">${member.name}</span> - ${member.bio}</li>`
