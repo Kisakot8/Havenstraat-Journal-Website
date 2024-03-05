22653a737c365cab2782c6dc3061dd502eded285
kisakot8 <kisakotdad@gmail.com>
2024-03-05 15:08:13 +0100

    Added mission statement (no styling)

--- src/_data/getSecrets.js
+++ src/_data/getSecrets.js
@@ -5,10 +5,12 @@ require('dotenv').config();
 function getSecrets () {
     const NAME = process.env.NAME;
     const INSTA_LINK = process.env.INSTA_LINK;
+    const MISSION_TEXT = process.env.MISSION_TEXT;
 
     return {
         "name": NAME,
-        "insta_link": INSTA_LINK
+        "insta_link": INSTA_LINK,
+        "mission_text": MISSION_TEXT
     }
 }
 
