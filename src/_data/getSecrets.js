dfb33dec4996cc3a30debe7032a88896ea5e0614
kisakot8 <kisakotdad@gmail.com>
2024-03-14 22:59:59 +0100

    Removed unnecessary environment variables

--- src/_data/getSecrets.js
+++ src/_data/getSecrets.js
@@ -6,13 +6,11 @@ function getSecrets () {
     const NAME = process.env.NAME;
     const INSTA_LINK = process.env.INSTA_LINK;
     const MISSION_TEXT = process.env.MISSION_TEXT;
-    const ADOBE_API = process.env.ADOBE_API;
 
     return {
         "name": NAME,
         "insta_link": INSTA_LINK,
         "mission_text": MISSION_TEXT,
-        "adobe_api": ADOBE_API
     }
 }
 
