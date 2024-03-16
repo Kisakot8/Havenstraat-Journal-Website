9d688fdec3580c853b535aef0f024a35eae73544
kisakot8 <kisakotdad@gmail.com>
2024-03-16 22:02:28 +0100

    Added contact email as environment variable

--- src/_data/getSecrets.js
+++ src/_data/getSecrets.js
@@ -6,11 +6,13 @@ function getSecrets () {
     const NAME = process.env.NAME;
     const INSTA_LINK = process.env.INSTA_LINK;
     const MISSION_TEXT = process.env.MISSION_TEXT;
+    const CONTACT_EMAIL = process.env.CONTACT_EMAIL;
 
     return {
         "name": NAME,
         "insta_link": INSTA_LINK,
         "mission_text": MISSION_TEXT,
+        "contact_email": CONTACT_EMAIL,
     }
 }
 
